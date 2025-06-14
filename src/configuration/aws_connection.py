09edd4a7c529c17cce9dee0471478dda1342ff46
Rakib Uddin <rakibuddinraki2003@gmail.com>
2025-06-14 11:33:48 +0600

    Remove hardcoded AWS credentials for security

--- src/configuration/aws_connection.py
+++ src/configuration/aws_connection.py
@@ -14,15 +14,13 @@ class S3Client:
         """
 
         if S3Client.s3_resource==None or S3Client.s3_client==None:
-            # Try to get credentials from environment variables first
             __access_key_id = os.getenv(AWS_ACCESS_KEY_ID_ENV_KEY)
             __secret_access_key = os.getenv(AWS_SECRET_ACCESS_KEY_ENV_KEY)
             
-            # If environment variables are not set, use hardcoded credentials
             if __access_key_id is None:
-                __access_key_id = "***REMOVED***"
+                raise Exception(f"Environment variable: {AWS_ACCESS_KEY_ID_ENV_KEY} is not not set.")
             if __secret_access_key is None:
-                __secret_access_key = "***REMOVED***"
+                raise Exception(f"Environment variable: {AWS_SECRET_ACCESS_KEY_ENV_KEY} is not set.")
             
             S3Client.s3_resource = boto3.resource('s3',
                                             aws_access_key_id=__access_key_id,
